6bdda9a45ca6c9233635da1d0a9b99e1bc152a05
123 <a850803tw@gmail.com>
2018-06-22 14:00:26 +0800

    Kendo_CRUD

--- Kendo/Kendo/App_Start/WebApiConfig.cs
+++ Kendo/Kendo/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Kendo
 {
@@ -14,11 +15,18 @@ namespace Kendo
             // Web API 路由
             config.MapHttpAttributeRoutes();
 
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            GlobalConfiguration.Configuration.Formatters.XmlFormatter.SupportedMediaTypes.Clear();
+
+            var xmlencoder = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(x => x.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(xmlencoder);
         }
     }
 }
--- Kendo/Kendo/Controllers/ProductsController.cs
+++ Kendo/Kendo/Controllers/ProductsController.cs
@@ -8,13 +8,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Kendo.Controllers
 {
-    [RoutePrefix("kendo")]
     public class ProductsController : ApiController
     {
-        [Route("")]
         [HttpGet]
         public IEnumerable<Products> Get()
         {
@@ -24,5 +23,57 @@ namespace Kendo.Controllers
 
             return products;
         }
+
+
+        [HttpPost]
+        public void Create(Products item)
+        {
+            var db = new SqlConnection(SqlConnectionString.ConnectionString());
+
+            db.Execute(
+                "INSERT INTO Products ( ProductName,  UnitPrice  , ShelfDate ,Description,CategoryID)" +
+                "VALUES ( @productName,  @unitPrice ,@shelfdate ,@description,@categoryID)", new
+                {
+                    
+                    productName = item.ProductName,
+                    unitPrice = item.UnitPrice ,
+                    shelfdate = item.ShelfDate ,
+                    description = item.Description,
+                    categoryID = item.CategoryID
+                });
+        }
+
+
+        [HttpPost]
+        public void Update([FromBody] Products item)
+        {
+            var db = new SqlConnection(SqlConnectionString.ConnectionString());
+
+            db.Execute("UPDATE Products " +"SET ProductName = @productName " +"WHERE ProductID = @productID", new
+
+                {
+                     productID=item.ProductID,
+                    productName = item.ProductName
+                });
+        }
+
+        
+        [HttpPost]
+        public void Destroy([FromBody] Products item)
+
+        {
+
+            var db = new SqlConnection(SqlConnectionString.ConnectionString());
+
+            db.Execute("delete Products where ProductID=@ProductID", new
+
+            {
+
+                ProductID = item.ProductID
+
+            });
+
+        }
+
     }
 }
